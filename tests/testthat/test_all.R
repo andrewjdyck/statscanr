8c2e705b80108fe0f1a7722e5fabc0909aae65ca
Andy <andrew@andrewdyck.com>
2020-05-12 09:35:39 -0600

    test of cube download and check_api()

--- tests/testthat/test_all.R
+++ tests/testthat/test_all.R
@@ -2,16 +2,28 @@
 context("Testing main functions")
 
 
-test_that(
-  'Download URL returns text', {
+check_api <- function() {
+  if (httr::GET('https://www150.statcan.gc.ca/t1/wds/rest/getCodeSets')$status_code != 200) {
+    skip("API not available")
+  }
+}
+
+test_that('Download URL returns text', {
+  check_api()
   download_url <- get_product_download_url('14100287')
   expect_that(download_url, is_a("character"))
 })
 
 
 test_that('Product metadata is a list', {
+  check_api()
   metadata <- get_product_metadata('14100287')
   expect_that(metadata, is_a("list"))
 })
 
+test_that('Product cube downloads', {
+  check_api()
+  pid <- '1310076601'
+  download_product_cube(pid)
+})
 
